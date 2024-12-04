f76c93609aaedfd3e3dda38871f2162d33146d93
St4ckkk <keyanandydelgado@gmail.com>
2024-12-04 12:00:21 +0800

    Add Tic-Tac-Toe game components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage"
+import Test from "./components/Test"
+import Board from "./components/tic-tac-toe/Square";
+
 const App = () => {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
+    <>
+      <Board />
+    </>
   );
 }
 
